7956f0ce181737a8bcbd199c0a80866e89c63073
Sasha Rush <srush@seas.harvard.edu>
2017-08-22 15:04:48 -0400

    .

--- push.sh
+++ push.sh
@@ -2,9 +2,8 @@ jinja2 index.html.tpl  site.yaml > index.html
 # j2 index.html.tpl site.yaml > index.html
 git add .
 git commit -a -m .
-git push origin gh-pages
-ssh cs281@login.fas.harvard.edu "cd public_html; git pull; chmod 755 *.html *.css"
-
+git push origin master
+# ssh cs281@login.fas.harvard.edu "cd public_html; git pull; chmod 755 *.html *.css"
 # Mark needed to install these:
 # - pip install pyyaml
 # - pip install jinja2-cli
