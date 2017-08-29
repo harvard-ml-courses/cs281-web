214bd980f3648fa1497ace6f48f2f68215635aaa
Sasha Rush <srush@seas.harvard.edu>
2017-08-29 15:21:26 -0400

    .

--- push_ch.sh
+++
@@ -1,6 +0,0 @@
-# jinja2 index.html.tpl  site.yaml > index.html
-j2 index.html.tpl site.yaml > index.html
-git add .
-git commit -a -m .
-git push origin gh-pages
-ssh cs181@login.fas.harvard.edu "cd public_html; git pull; chmod 755 *.html *.css"
